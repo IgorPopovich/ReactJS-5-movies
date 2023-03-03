001a8e56ea4187765bb4ef000d558545b7637399
Igor_Popovich <78104120+IgorPopovich@users.noreply.github.com>
2023-03-03 08:35:50 +0200

    Update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,40 +1,43 @@
 import PropTypes from 'prop-types';
-import React, {useEffect, useState} from 'react';
+import React, {Component} from 'react';
 import { Link } from "react-router-dom";
 import Loader from "../Loader/Loader";
 import css from './Home.module.css';
 
-const Home = ({ getId }) => {
-  const [items, setItems] = useState([])
-  const [showLoader, setShowLoader] = useState(false)
-//eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => {
-      setShowLoader(true)
-      fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=d3f7b1c0656b5d6ae1aec003a1724af6`)
-      .then(response => response.json())
-      .then(data => {
-        setItems(data.results)
-      })
-      setShowLoader(false)
-      
-    });
-
-  const updateId = (name) => {
-    getId(name)
+export default class Home extends Component {
+  state = {
+    items: null,
+    showLoader: false,
   }
 
-  return (
+  updateId = (name) => {
+    this.props.getId(name)
+  }
+
+  componentDidMount() {
+    this.setState({ showLoader: true })
+    fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=d3f7b1c0656b5d6ae1aec003a1724af6`)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({ items: data.results })
+    })
+    this.setState({ showLoader: false })
+  }
+  
+  render () {
+    return (
     <div className={css.container}>
       <ul className={css.items}>
-        {showLoader && <Loader />}
-            {items.length > 0 && items.map(( item, index ) => (
-              <Link onClick={() => updateId(item.id)} to={`/movies/:${item.id}`} className={css.item} key={index}>
+        {this.state.showLoader && <Loader />}
+            {this.state.items && this.state.items.map(( item, index ) => (
+              <Link onClick={() => this.updateId(item.id)} to={`/movies/:${item.id}`} className={css.item} key={index}>
                 {item.title}
               </Link>
             ))}
       </ul>
     </div>
-  )
+    )
+  }
 }
 
 Home.propTypes = {
@@ -42,5 +45,3 @@ Home.propTypes = {
   showLoader: PropTypes.bool,
   items: PropTypes.array,
 };
-
-export default Home;
